0312a7d4e039f4a0dcb447c6c2455d7dcaf0dd00
Kulebyaka1337 <sergeevserg032@gmail.com>
2025-09-30 17:47:16 +0300

    Task03

---
+++ Task03/guess-number/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mr-wo/guess-number',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5f390de4343dfeaced81c8d96184faa24a16a44f',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mr-wo/guess-number' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5f390de4343dfeaced81c8d96184faa24a16a44f',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wp-cli/php-cli-tools' => array(
+            'pretty_version' => 'v0.12.6',
+            'version' => '0.12.6.0',
+            'reference' => 'f12b650d3738e471baed6dd47982d53c5c0ab1c3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/php-cli-tools',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
